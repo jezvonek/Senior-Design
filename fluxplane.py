999f26c5662ea1c789489dd72ab60c3126e22167
Frank Popelar <45672827+FrankPopelar@users.noreply.github.com>
2020-05-03 01:04:20 -0500

    minor bugfixes on the flux plane generation

--- fluxplane.py
+++ fluxplane.py
@@ -2,8 +2,8 @@ import math as m
 import numpy as n
 
 """
-Closest Object X
-find the x coord of the potential leak source closest to the current position of the drone
+Closest Object
+find the coords of the potential leak source closest to the current position of the drone
 inputs      x           - the current x position of the drone (m)
             y           - the current y position of the drone (m)
             z           - the current z position of the drone (m)
@@ -11,13 +11,13 @@ inputs      x           - the current x position of the drone (m)
 outputs     cObj        - 1D array of the coordinates of the closest object (m) (x=0,y=1,z=2)
 """
 
-def getClosestObjectX(x, y, z, objs):
+def getClosestObject(x, y, z, objs):
 
     # ----------------------
     # Variable Instantiation
     # ----------------------
 
-    cObj = n.zeros([1,3])                                 # preallocate the cObj array
+    cObj = n.zeros([3])                                 # preallocate the cObj array
     shortest = float('inf')                             # the current shortest distance
     closestObj = 0                                      # the index (ID) of the closest object to the drone
 
@@ -113,7 +113,7 @@ def dispersion(stability, xbar):
     # Variable Instantiation
     # ----------------------
 
-    sigma = n.zeros([1,2])
+    sigma = n.zeros([2])
     xbar = xbar / 1000.0            # convert xbar from m to km
 
     # ---------------
@@ -131,14 +131,14 @@ def dispersion(stability, xbar):
         T = 8.3333 - 0.72382*m.log(xbar)
 
     # compute sigma_y
-    sigma[0] = 1000 * xbar * m.tan(T*pi/180.0)/2.15
+    sigma[0] = 1000 * xbar * m.tan(T*m.pi/180.0)/2.15
 
     # ---------------
     # Finding sigma_z
     # ---------------
 
     # first, find a and b based off of the stability condition
-    "NOTE: this approach assumes a distance of >= 100m to the closest potential source"
+    "NOTE: this approach assumes a distance of <= 100m to the closest potential source"
     if stability == 'A':
         a = 122.8
         b = 0.9447
@@ -178,18 +178,17 @@ def outline(bounds, res):
     for i in range(0, int((n.shape(wps)[0]-2)/2)):                                      # loop over the waypoints in pairs of left and right save the last pair
         for j in range(0,2):                                                            # loop over the x and y coordinates
             wps[2*i,j] = bounds[1 + (i%2),j]
-            wps[2*i+1,j] = bounds[1 + ((i+1)&2),j]
+            wps[2*i+1,j] = bounds[1 + ((i+1)%2),j]
 
-        wps[2*i,2] = bounds[1,3] - res*i                                                # fill in the z coordinate
-        wps[2*i+1,2] = bounds[1,3] - res*i
+        wps[2*i,2] = bounds[1,2] - res*i                                                # fill in the z coordinate
+        wps[2*i+1,2] = bounds[1,2] - res*i
 
-    switcher = (n.shape(wps)[0]/2)%2                                                    # find whether there is an even or odd number of rows
-    wps[n.shape(wps)[0]-2, 0] = bounds[switcher + 1, 0]                                 # fill in the last rows
-    wps[n.shape(wps)[0]-2, 1] = bounds[switcher + 1, 1]
-    wps[n.shape(wps)[0]-2, 0] = bounds[0, 2]
-    wps[n.shape(wps)[0]-1, 0] = bounds[(switcher + 1)%2, 0]
-    wps[n.shape(wps)[0]-1, 0] = bounds[(switcher + 1)%2, 1]
-    wps[n.shape(wps)[0]-1, 0] = bounds[0, 2]
+    wps[n.shape(wps)[0]-2, 0] = wps[n.shape(wps)[0]-3, 0]                               # fill in the last rows
+    wps[n.shape(wps)[0]-2, 1] = wps[n.shape(wps)[0]-3, 1]
+    wps[n.shape(wps)[0]-2, 2] = bounds[0, 2]
+    wps[n.shape(wps)[0]-1, 0] = wps[n.shape(wps)[0]-4, 0]
+    wps[n.shape(wps)[0]-1, 1] = wps[n.shape(wps)[0]-4, 1]
+    wps[n.shape(wps)[0]-1, 2] = bounds[0, 2]
 
     return wps
 
@@ -213,13 +212,13 @@ inputs      x           -   the current x position of the drone (m)
 outputs     wps         -   2D array containing the (x,y,z) for each waypoint in the flux plane
 """
 
-def fluxPlane(x, y, z, objs, v, theta, temp, overcast=False, res):
+def fluxPlane(x, y, z, objs, v, theta, temp, res, overcast=False):
     
     # -----------------------------------
     # Finding the Dispersion Coefficients
     # -----------------------------------
     
-    closeObj = getClosestObjectX(x,y,z,obj)             # find the coordinates of the closest potential leak source
+    closeObj = getClosestObject(x,y,z,obj)             # find the coordinates of the closest potential leak source
     xbar = m.sqrt((closeObj[0] - x)**2 + (closeObj[1] - y)**2 + (closeObj[2] - z)**2)          # the distance between the drone and the closest potential leak source
     stability = getStab(v, temp, overcast)              # find the Pasquill Stability Class
     sigma = dispersion(stability, xbar)                 # find the y and z dispersion coefficients, which are the bounds of the flux plane
@@ -229,19 +228,19 @@ def fluxPlane(x, y, z, objs, v, theta, temp, overcast=False, res):
     # ------------------------------------
 
     # rotate the flux plane to be perpindicular to the wind direction
-    xhat1 = x - sigma[0]*m.sin(theta)                   # the x and y coordinates of the flux plane bounds
-    yhat1 = y + sigma[0]*m.cos(theta)
-    xhat2 = x + sigma[0]*m.sin(theta)
-    yhat2 = y + sigma[0]*m.cos(theta)
-
+    xhat1 = x - sigma[0]*m.sin((360-theta)*m.pi/180)                   # the x and y coordinates of the flux plane bounds
+    yhat1 = y + sigma[0]*m.cos((360-theta)*m.pi/180)
+    xhat2 = x + sigma[0]*m.sin((360-theta)*m.pi/180)
+    yhat2 = y + sigma[0]*m.cos((360-theta)*m.pi/180)
+    
     # z coordinates for the flux plane bounds
-    if z - sigma[1] < 4.0:                              # safety check for zhat1 so that the drone maintains height above the ground 
-        zhat1 = 4.0
+    if z - sigma[1] < 0.0: 
+        zhat1 = 0.0
     else:
         zhat1 = z - sigma[1]
 
     zhat2 = z + sigma[1]
-
+    
     # filling in the bounds array
     bounds = [[xhat1, yhat1, zhat1], [xhat1, yhat1, zhat2], [xhat2, yhat2, zhat1], [xhat2, yhat2, zhat2]]
 
