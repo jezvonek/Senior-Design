2c391876c5a25769250258b7458c0cee4a32fb3e
Frank Popelar <45672827+FrankPopelar@users.noreply.github.com>
2020-05-03 16:43:34 -0500

    different method of initial waypoint finding
    maybe more rigorous??

--- initialwaypoint.py
+++ initialwaypoint.py
@@ -18,32 +18,43 @@ def initwaypoint(prob, xspacing, yspacing, zspacing, power=1):
     # ----------------------
 
     first = True                                                                                        # boolean value of whether this is the first entry in the waypoints array
-    comp = n.empty([n.shape(prob)[0], n.shape(prob)[1], n.shape(prob)[2], power], dtype = bool)         # preallocate the comparison array
+    comp = n.empty([n.shape(prob)[0], n.shape(prob)[1], n.shape(prob)[2]], dtype = bool)                # preallocate the comparison array
+    # fill the comp array
+    for i in range(0,n.shape(prob)[0]):                     
+        for j in range(0,n.shape(prob)[1]):
+            for k in range(0,n.shape(prob)[2]):
+                comp[i,j,k] = True
+    wps = n.zeros([1,3])                                                                              # instantiate the wps array
 
     # ------
     # Engine
     # ------
-    
+
     # perform the comparisons for each power level
-    for i in range(0,power):
-        comp[:,:,:,i] = ((prob >= n.roll(prob, i+1, 0)) & (prob >= n.roll(prob, -1*(i+1), 0)) & (prob >= n.roll(prob,  i+1, 1)) & (prob >= n.roll(prob, -1*(i+1), 1)) & (prob >= n.roll(prob,  i+1, 2)) & (prob >= n.roll(prob, -1*(i+1), 2)))
+    for i in range(0,n.shape(prob)[0]):                     # loop over the points
+        for j in range(0,n.shape(prob)[1]):
+            for k in range(0,n.shape(prob)[2]):
+                for x in range(-1*power, power + 1):        # loop over the window
+                    compx = i + x
+                    if (compx >= 0) and (compx < n.shape(prob)[0]):
+                        for y in range(-1*power, power + 1):
+                            compy = j + y
+                            if (compy >= 0) and (compy < n.shape(prob)[1]):
+                                for z in range(-1*power, power + 1):
+                                    compz = k + z
+                                    if (compz >= 0) and (compz < n.shape(prob)[2]):
+                                        if comp[i,j,k]:
+                                            comp[i,j,k] = comp[i,j,k] & (prob[i,j,k] >= prob[compx, compy, compz])
 
     # find the coordinates of the maxima based on the indices of the comparison array
     for i in range(0,n.shape(prob)[0]):                             # loop over the coordinate indices
         for j in range(0, n.shape(prob)[1]):
             for k in range(0, n.shape(prob)[2]):
-                # find whether the index is a local maxima by &ing all the individual coordinate layers of the comparison matrix together
-                temp = comp[i,j,k,0]
-                if not(power == 1):
-                    for l in range(0, power):                       # loop over the window sizes if greater than 1
-                        temp = temp & comp[i,j,k,l]
-
-                # if the point is a local maxima, add to waypoints array
-                if temp:
-                    if first:                                                                           # if this is the first entry in wps, create wps
+                if comp[i,j,k]:
+                    if first:
                         first = False
                         wps = n.array([[i*xspacing, j*yspacing, k*zspacing]])
                     else:
-                        wps = n.append(wps, [[i*xspacing, j*yspacing, k*zspacing]], axis=0)
+                        wps = n.append(wps, [[i*xspacing, j*yspacing, k*zspacing]], axis=0)     
 
     return wps
